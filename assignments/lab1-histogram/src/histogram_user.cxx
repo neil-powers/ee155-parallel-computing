5fca2af0aa8468d601a586639699a9a031ef890c
Neil Powers <neil.powers@tufts.edu>
2024-04-24 16:12:27 -0400

    lab1: histogram completed. Beat reference

--- assignments/lab1-histogram/src/histogram_user.cxx
+++ assignments/lab1-histogram/src/histogram_user.cxx
@@ -7,6 +7,11 @@
 #include <pthread.h>
 #include "ee155_utils.hxx"
 
+using namespace std;
+
+static void th_func(int me, std::vector<int> &final_hist,
+                     const std::vector<int> &input_data, int n_threads);
+
 // How many buckets the histogram has.
 extern const int N_BUCKETS;
 static std::mutex g_mut;        // For printing with LOGM
@@ -17,7 +22,18 @@ static std::mutex g_mut;        // For printing with LOGM
 //************************************************************************
 std::vector<int> compute_multithread
         (const std::vector<int> &input_data, int n_threads) {
-    
+    vector<int> final_hist(N_BUCKETS, 0);
+    vector<thread> threads; threads.reserve(n_threads);
+
+    for (int i = 0; i < n_threads; i++) {
+        threads.push_back(thread(th_func, i, ref(final_hist), cref(input_data), n_threads));
+    }
+
+    for (int i = 0; i < n_threads; i++) {
+        threads[i].join();
+    }
+
+    return final_hist;
 }
 
 
@@ -25,7 +41,23 @@ std::vector<int> compute_multithread
 // You write this function yourself also. It gets instantiated multiple times
 // in different threads.
 //************************************************************************
-static void th_func (int me, std::vector<int> &final_hist,
+static void th_func(int me, std::vector<int> &final_hist,
                      const std::vector<int> &input_data, int n_threads) {
+    static std::mutex mut; // For adding data to the histogram
 
+    vector<int> hist(N_BUCKETS, 0);
+
+    int region_size = input_data.size() / n_threads;
+    int start = me * region_size;
+    int end = (me == n_threads - 1) ? input_data.size() : (me + 1) * region_size;
+
+    for (int i = start; i < end; i++) {
+        hist[input_data[i]]++;
+    }
+
+    mut.lock();
+    for (int i = 0; i < N_BUCKETS; i++) {
+        final_hist[i] += hist[i];
+    }
+    mut.unlock();
 }
